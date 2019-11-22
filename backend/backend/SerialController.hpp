e16872c197fbe0d09c7365e26753112f9cb76d69
StephanXu <xuzihanapple@live.com>
2019-11-23 02:47:11 +0800

    增加后端实现

---
+++ backend/backend/SerialController.hpp
@@ -0,0 +1,54 @@
+#ifndef SERIALCONTROLLER_HPP
+#define SERIALCONTROLLER_HPP
+
+#include <cpprest/http_listener.h>
+#include <cpprest/http_msg.h>
+
+#include <iostream>
+#include <iterator>
+#include <string>
+
+using namespace std;
+using namespace web;
+using namespace http;
+using namespace utility;
+using namespace http::experimental::listener;
+
+class SerialController
+{
+public:
+	SerialController()
+	{
+	}
+	SerialController(utility::string_t url) :m_Listener(url)
+	{
+		m_Listener.support(methods::GET, std::bind(&SerialController::Get, this, std::placeholders::_1));
+	}
+	virtual ~SerialController()
+	{
+	}
+
+	pplx::task<void>open() { return m_Listener.open(); }
+	pplx::task<void>close() { return m_Listener.close(); }
+private:
+	void Get(http_request message)
+	{
+		message.extract_json().then(
+			[=](pplx::task<web::json::value> t) {
+				try
+				{
+					auto res = t.get();
+					ucout << res.to_string() << std::endl;
+					message.reply(status_codes::OK, "ok", "text");
+				}
+				catch (...)
+				{
+					//...
+				}
+			}
+		);
+	}
+
+	http_listener m_Listener;
+};
+#endif //SERIALCONTROLLER_HPP
---
+++ backend/backend/backend.cpp
@@ -0,0 +1,35 @@
+﻿// backend.cpp: 定义应用程序的入口点。
+//
+
+#include "backend.h"
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include "SerialController.hpp"
+#include <memory>
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
+std::unique_ptr<SerialController> serialController{};
+
+int main()
+{
+	utility::string_t port = U("34500");
+	utility::string_t addr = U("http://127.0.0.1:");
+	utility::string_t path = U("/api");
+	addr.append(port).append(path);
+	serialController = std::make_unique<SerialController>(addr);
+	serialController->open().wait();
+
+	ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
+
+	std::string line;
+	std::getline(std::cin, line);
+
+	serialController->close().wait();
+	return 0;
+}
